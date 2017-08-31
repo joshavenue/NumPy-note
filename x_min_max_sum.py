2a7b814061119f3407303abadf60cfee9708e630
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-01 03:25:41 +0800

    Create x_min_max_sum.py

---
+++ x_min_max_sum.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+
+x = np.random.random((3,3))       // create a 3 by 3 matrix with randoms number within //
+
+print(x.sum())                    // Find the summation of all the numbers within the array //
+print(x.min())                    // Find the minumum value //
+print(x.max())                    // What else do you think this does? //
