50fbdbeeb2ef1290671f425c634c9401434dcd68
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-01 03:52:05 +0800

    Update x_min_max_sum.py

--- x_min_max_sum.py
+++ x_min_max_sum.py
@@ -6,3 +6,7 @@ x = np.random.random((3,3))       // create a 3 by 3 matrix with randoms number 
 print(x.sum())                    // Find the summation of all the numbers within the array //
 print(x.min())                    // Find the minumum value //
 print(x.max())                    // What else do you think this does? //
+
+print(x.cumsum())                 // prints the cumulative of each row , try it and you will know //
+
+print(x.sum(axis=0))              // prints the sums of the first row, axis is the row number //
