f9d39e66da169da62bd5af9520828e386434bcbf
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-01 01:54:27 +0800

    Create np.arange_reshape.py

---
+++ np.arange_reshape.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x = np.arange(20).reshape(4,5)  // x = np.arange(num1).reshape(num2,num3) , whereby num1 is the range of the number
+                                // num2 is the vertical axis, num3 is the horizontal axis length
+print(x)
+
+
