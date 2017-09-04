39d6f46cf2f03fd2751533f5045f3fafa92ca546
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-04 11:11:19 +0800

    Update dot_shape.py

--- dot_shape.py
+++ dot_shape.py
@@ -5,3 +5,8 @@ x = np.arange(0,100,2).reshape(2,2,5)
 print(x)
 
 print(x.shape)                  // Find shape if not mentioned.
+
+for x in c.flat:                // .flat flattens every data into a straight line
+    print(x)
+    
+    
